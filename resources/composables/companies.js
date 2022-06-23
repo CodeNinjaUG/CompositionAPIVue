e6db15d337b2460aa4b1ab9cef9c01dbc5af5bab
codeninjaug <segayiandrew@gmail.com>
2022-06-23 13:58:52 +0300

    composition api

---
+++ resources/composables/companies.js
@@ -0,0 +1,64 @@
+import { ref } from 'vue'
+import axios from "axios"
+import {useRouter } from 'vue-router';
+
+export default function useCompanies(){
+    const companies = ref([])
+    const company = ref([])
+    const router = useRouter()
+    const errors  = ref('')
+    const getCompanies = async() =>{
+        let response = await axios.get('/api/companies')
+        companies.value = response.data.data
+    }
+
+    const getCompany = async(id) =>{
+        let response = await axios.get('/api/companies/'+id)
+        company.value = response.data.data
+    }
+
+
+    const destroyCompany = async(id) =>{
+         await axios.delete('/api/companies/'+id)
+         alert('deleted');
+    }
+
+    const storeCompany = async(data) =>{
+        errors.value = ''
+        try{
+            await axios.post('/api/companies/',data)
+            await router.push({name: 'companies.index'})
+        }catch(e){
+            if(e.response.status === 422){
+                for( const key in e.response.data.errors){
+                    errors.value += e.response.data.errors[key][0] + '';
+                }
+            }
+        }
+
+   }
+   const UpdateCompany = async(id) =>{
+    errors.value = ''
+    try{
+        await axios.put('/api/companies/'+id, company.value)
+        await router.push({name: 'companies.index'})
+    }catch(e){
+        if(e.response.status === 422){
+            for( const key in e.response.data.errors){
+                errors.value += e.response.data.errors[key][0] + '';
+            }
+        }
+    }
+
+}
+    return {
+        companies, 
+        errors,
+        getCompany,
+        company,
+        UpdateCompany,
+        getCompanies,
+        storeCompany,
+        destroyCompany
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
+import { createApp } from "vue";
+import router from '../router';
+import CompaniesIndex from '../components/companies/CompaniesIndex';
+createApp({
+    components: {
+        CompaniesIndex
+    }
+}).use(router).mount('#app')
